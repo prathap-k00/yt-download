2effaa5544fdab026d1815dd81a060eff3fd59be
Prathap Kulal <prathapkual258@gmail.com>
2026-02-20 20:46:38 +0530

    potoken fix2

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def download():
             flash("Invalid or empty URL", "danger")
             return redirect(url_for("home"))
 
-        yt = YouTube(url)
+        yt = YouTube(url,use_po_token=True,client='web')
         safe_title = secure_filename(yt.title)
 
         # VIDEO
